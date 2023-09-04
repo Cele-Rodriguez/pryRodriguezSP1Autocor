49ec301cd6de0073617495a33da86319f8b7e2d8
CELU0895 <CELU0895@ies21.onmicrosoft.com>
2023-09-04 01:27:58 -0300

    Interfaz grafica completa

--- pryRodriguezSP1/frmMain.Designer.cs
+++ pryRodriguezSP1/frmMain.Designer.cs
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.grbRepuesto = new System.Windows.Forms.GroupBox();
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -44,62 +44,66 @@
             this.cboMarca = new System.Windows.Forms.ComboBox();
             this.rdbNacional = new System.Windows.Forms.RadioButton();
             this.rdbImportado = new System.Windows.Forms.RadioButton();
-            this.groupBox1.SuspendLayout();
+            this.grbRepuesto.SuspendLayout();
             this.SuspendLayout();
             // 
-            // groupBox1
+            // grbRepuesto
             // 
-            this.groupBox1.Controls.Add(this.rdbImportado);
-            this.groupBox1.Controls.Add(this.rdbNacional);
-            this.groupBox1.Controls.Add(this.cboMarca);
-            this.groupBox1.Controls.Add(this.label5);
-            this.groupBox1.Controls.Add(this.txtPrecio);
-            this.groupBox1.Controls.Add(this.txtNom);
-            this.groupBox1.Controls.Add(this.txtCod);
-            this.groupBox1.Controls.Add(this.label4);
-            this.groupBox1.Controls.Add(this.label3);
-            this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(34, 12);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(249, 251);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
+            this.grbRepuesto.Controls.Add(this.rdbImportado);
+            this.grbRepuesto.Controls.Add(this.rdbNacional);
+            this.grbRepuesto.Controls.Add(this.cboMarca);
+            this.grbRepuesto.Controls.Add(this.label5);
+            this.grbRepuesto.Controls.Add(this.txtPrecio);
+            this.grbRepuesto.Controls.Add(this.txtNom);
+            this.grbRepuesto.Controls.Add(this.txtCod);
+            this.grbRepuesto.Controls.Add(this.label4);
+            this.grbRepuesto.Controls.Add(this.label3);
+            this.grbRepuesto.Controls.Add(this.label2);
+            this.grbRepuesto.Controls.Add(this.label1);
+            this.grbRepuesto.Location = new System.Drawing.Point(24, 12);
+            this.grbRepuesto.Name = "grbRepuesto";
+            this.grbRepuesto.Size = new System.Drawing.Size(260, 251);
+            this.grbRepuesto.TabIndex = 0;
+            this.grbRepuesto.TabStop = false;
+            this.grbRepuesto.Text = "Repuesto";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.Location = new System.Drawing.Point(19, 171);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.Size = new System.Drawing.Size(42, 15);
             this.label4.TabIndex = 3;
             this.label4.Text = "Precio";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(19, 126);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.Size = new System.Drawing.Size(42, 15);
             this.label3.TabIndex = 2;
             this.label3.Text = "Marca";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(19, 81);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.Size = new System.Drawing.Size(52, 15);
             this.label2.TabIndex = 1;
             this.label2.Text = "Nombre";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(19, 36);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.Size = new System.Drawing.Size(46, 15);
             this.label1.TabIndex = 0;
             this.label1.Text = "Codigo";
             // 
@@ -163,9 +167,10 @@
             // label5
             // 
             this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.Location = new System.Drawing.Point(19, 216);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.Size = new System.Drawing.Size(44, 15);
             this.label5.TabIndex = 7;
             this.label5.Text = "Origen";
             // 
@@ -180,9 +185,10 @@
             // rdbNacional
             // 
             this.rdbNacional.AutoSize = true;
-            this.rdbNacional.Location = new System.Drawing.Point(94, 216);
+            this.rdbNacional.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdbNacional.Location = new System.Drawing.Point(94, 214);
             this.rdbNacional.Name = "rdbNacional";
-            this.rdbNacional.Size = new System.Drawing.Size(67, 17);
+            this.rdbNacional.Size = new System.Drawing.Size(74, 19);
             this.rdbNacional.TabIndex = 9;
             this.rdbNacional.TabStop = true;
             this.rdbNacional.Text = "Nacional";
@@ -191,9 +197,10 @@
             // rdbImportado
             // 
             this.rdbImportado.AutoSize = true;
-            this.rdbImportado.Location = new System.Drawing.Point(167, 214);
+            this.rdbImportado.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdbImportado.Location = new System.Drawing.Point(168, 214);
             this.rdbImportado.Name = "rdbImportado";
-            this.rdbImportado.Size = new System.Drawing.Size(72, 17);
+            this.rdbImportado.Size = new System.Drawing.Size(81, 19);
             this.rdbImportado.TabIndex = 10;
             this.rdbImportado.TabStop = true;
             this.rdbImportado.Text = "Importado";
@@ -203,23 +210,23 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(426, 283);
+            this.ClientSize = new System.Drawing.Size(423, 283);
             this.Controls.Add(this.btnSalir);
             this.Controls.Add(this.btnConsultar);
             this.Controls.Add(this.btnCancelar);
             this.Controls.Add(this.btnAceptar);
-            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.grbRepuesto);
             this.Name = "frmMain";
             this.Text = "AUTOCOR - Repuestos";
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
+            this.grbRepuesto.ResumeLayout(false);
+            this.grbRepuesto.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox grbRepuesto;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
